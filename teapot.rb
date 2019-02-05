35942061de61aaf75fc4f9494c535caeda65d42e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-06 10:46:09 +1300

    Update to use xenial which has working compilers.

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "2.0"
+teapot_version "2.3"
 
 define_target "generate-travis" do |target|
 	target.description = <<-EOF
